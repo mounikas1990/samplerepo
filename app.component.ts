842c14f9a2af6c74efa65e48ee18d591f52f4a0a
mounika <mounikas1990@gmail.com>
2018-04-08 20:57:11 -0400

    added files to commit

---
+++ app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ /* title = 'app';
+  post ={
+    title:'app',
+    isFavourite:true,
+    count:10
+  }
+  onChange(){
+    console.log("Favourite is changed");
+  }
+
+  tweet={
+    body:'here is the body of tweet', 
+    likesCount:10,
+    isLiked:true,
+  }*/
+}
